47ac70d56eaec8080ea7af3e260803bb17bfb373
Cactus <ysjungman@gmail.com>
2023-06-29 10:53:56 +0900

    10_Coupling

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/SuperContraGame.java
@@ -0,0 +1,19 @@
+package com.in28minutes.learnspringframework.game;
+
+public class SuperContraGame {
+	public void up() {
+		System.out.println("top");
+	}
+	
+	public void down() {
+		System.out.println("sit down");
+	}
+	
+	public void left() {
+		System.out.println("Start");
+	}
+	
+	public void right() {
+		System.out.println("Shoot a bullet");
+	}
+}
